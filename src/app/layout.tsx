908fa69907569ad6d2690fe21e487d028fdfdf99
Danila Vaganov <danilavaganov41@gmail.com>
2026-01-04 21:46:33 +0600

    update metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Metadata } from 'next'
 import './globals.css'
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
 	title: 'Nexo',
@@ -19,16 +8,23 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
 	children,
-}: Readonly<{
+}: {
 	children: React.ReactNode
-}>) {
+}) {
 	return (
 		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+			<head>
+				<meta property='og:title' content='Nexo' />
+				<meta property='og:description' content='Social media app' />
+				<meta property='og:image' content='/favicon.png' />
+				<meta property='og:type' content='website' />
+				<meta property='og:url' content='https://nexo-inky.vercel.app/' />
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta name='twitter:title' content='Nexo' />
+				<meta name='twitter:description' content='Social media app' />
+				<meta name='twitter:image' content='/og-image.png' />
+			</head>
+			<body>{children}</body>
 		</html>
 	)
 }
