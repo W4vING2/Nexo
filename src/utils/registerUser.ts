1524eacde32774daf16116dfec21c622a8fe61de
Danila Vaganov <danilavaganov41@gmail.com>
2026-01-04 20:12:06 +0600

    fix

--- src/utils/registerUser.ts
+++ src/utils/registerUser.ts
@@ -5,12 +5,14 @@ import prisma from '../../lib/prisma'
 export default async function registerUser(formData: FormData) {
 	const email = formData.get('email') as string
 	const password = formData.get('password') as string
+	const name = formData.get('username') as string
 	if (!email || !password) throw new Error('Email and password are required')
-	console.log('User was registered:', { email, password })
+	console.log('User was registered:', { email, password, name })
 	await prisma.user.create({
 		data: {
 			email,
 			password,
+			name,
 		},
 	})
 }
